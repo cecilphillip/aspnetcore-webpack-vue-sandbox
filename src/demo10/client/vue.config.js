06820a40347fe5c1fbaa9ddcf15859d16bc42654
Cecil L. Phillip <cecilphillip@gmail.com>
2018-06-28 00:39:49 -0400

    demo 10 - customize webpack entry and outputs with

--- src/demo10/client/vue.config.js
+++ src/demo10/client/vue.config.js
@@ -3,6 +3,13 @@ module.exports = {
     // Include Vue runtime + compiler since we're using Vue directly within Razor
     runtimeCompiler: true,
     chainWebpack: config => {
+        config.entryPoints.delete('app');
+        config.entry('main').add('./src/main.ts');
+        config.output.when(process.env.NODE_ENV === 'production',
+            config => config.filename('[name].build.min.js'),
+            config => config.filename('[name].build.js')
+        );
+
         // disable generation of index.html to outputDir
         config.plugins.delete('html');
         config.plugins.delete('preload');
